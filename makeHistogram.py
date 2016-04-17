3fb1a231414f1a5facce3fec7eb86aee1f63fcb5
Lucio Dery <ldery@stanford.edu>
2016-04-16 21:27:35 -0700

    updated images with one colorbar

--- makeHistogram.py
+++ makeHistogram.py
@@ -3,22 +3,23 @@ import matplotlib.pyplot as plt
 import glob
 from matplotlib.backends.backend_pdf import PdfPages
 
-NUM_BUCKETS = 30
+NUM_BUCKETS = 60
 PT_INDEX = 0
 
-tracks = glob.glob("tracks2/*.csv")
+tracks = glob.glob("tracks/*.csv")
 
 def makeHistogram(filename):
     matrix = np.genfromtxt(filename, delimiter=",", skip_header=1)
-    pt_values = matrix[:, PT_INDEX]
+    pt_values = matrix[:, PT_INDEX]/1000  #convert to MeV
     title = filename.split("/")[1].split(".")[0]
     plt.hist(pt_values, bins=NUM_BUCKETS)
     plt.title(title)
+    plt.xlabel('pT[GeV]')
+    plt.ylabel('Number of Tracks')
     pp = PdfPages("Histograms/" + title + '.pdf')
     plt.savefig(pp, format='pdf')
     pp.close()
     plt.close()
-
 
 for filename in tracks:
     makeHistogram(filename)
--- makeImageFlagged.py
+++ makeImageFlagged.py
@@ -10,6 +10,7 @@ ETA_INDEX = 1
 
 my_cmap = matplotlib.cm.get_cmap('jet')
 my_cmap.set_under('white')
+titles  = ["Background", "BJet", "BBellipse", "VBFJet"]
 def makeImage(filename):
     matrix = np.genfromtxt(filename, delimiter=",", skip_header=1)
     vbf = matrix[matrix[:, TYPE_INDEX] == 1]
@@ -25,32 +26,30 @@ def makeImage(filename):
     x_positive, x_negative =  2.0*(2.5 - min_eta)/(max_eta - min_eta) - 1.0, 2.0*(-2.5 - min_eta)/(max_eta - min_eta) - 1.0
     x_edges, y_edges = np.linspace(x_negative, x_positive, 50), np.linspace(-3.2, 3.16, 63)
     x_ticks = np.linspace(x_negative, x_positive, 6)
-    plt.subplot(2, 2, 1)
-    generate_plot(background, x_edges, y_edges, x_ticks, "Background")
-    plt.subplot(2, 2, 2)
-    generate_plot(bjet, x_edges, y_edges,x_ticks, "Bjet")
-    plt.subplot(2, 2, 3)
-    generate_plot(bbellipse, x_edges, y_edges, x_ticks, "BBellipse")
-    plt.subplot(2, 2, 4)
-    generate_plot(vbf, x_edges, y_edges, x_ticks, "VBFJet")
-    plt.tight_layout()
+    fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(7,7))
+    packaged_matrix = [background, bjet, bbellipse, vbf]
+    for data, name, ax in zip(packaged_matrix, titles, axes.flat):
+        result, x_end, y_end = np.histogram2d(data[:, 1], data[:, 2], bins=(x_edges, y_edges), range=None, normed=False, weights=data[:, 0])
+        im = ax.imshow(np.flipud(result.T), cmap=my_cmap, vmin=0.1, interpolation="nearest")
+        ax.set_xticks(np.linspace(0, 49, 6))
+        ax.set_yticks(np.linspace(0, 62, 6))
+        ax.set_xticklabels([str(int(x*10)/10.0) for x in x_ticks])
+        ax.set_yticklabels(('-3.15', '-1.89', '-0.63', '0.63', '1.89', '3.15'))
+        ax.set_xlabel('Rapidity[eta]')
+        ax.set_ylabel('Azimuthal Angle[phi]')
+        ax.set_title(name)
+
+    fig.subplots_adjust(left=0.05, bottom=None, right=0.9, top=None, wspace=0.1, hspace=3)
+    cax = fig.add_axes([0.87, 0.101, 0.02, 0.82])
+    cbar = fig.colorbar(im, cax=cax)
+    cbar.ax.set_ylabel('pT')
+    fig.tight_layout()
     title = filename.split("/")[1].split(".")[0]
     pp = PdfPages("subevent_images/" + title + ".pdf")
     plt.savefig(pp, format="pdf")
     plt.close()
     pp.close()
 
-
-def generate_plot(mat, x_edges, y_edges, x_ticks, title):
-    result, x_end, y_end = np.histogram2d(mat[:, 1], mat[:, 2], bins=(x_edges, y_edges), range=None, normed=False, weights=mat[:, 0])
-    plt.xticks(np.linspace(0, 49, 6), [str(int(x*10)/10.0) for x in x_ticks])
-    plt.yticks(np.linspace(0, 62, 6), ('-3.15', '-1.89', '-0.63', '0.63', '1.89', '3.15'))
-    plt.pcolormesh(np.flipud(result.T), cmap=my_cmap, vmin=0.1)
-    plt.xlabel('Rapidity[eta]')
-    plt.ylabel('Azimuthal Angle[phi]')
-    cbar = plt.colorbar()
-    cbar.ax.set_ylabel('pT')
-    plt.title(title)
 
 for filename in glob.glob("tracks/*.csv"):
     makeImage(filename)
